f5b8a4369487f8d0d579b21ce880ad5ea1ce3cdb
electrodog10 <lewis@lewisproductions.com>
2015-07-06 11:08:40 -0500

    First Version
    First version of code!

---
+++ radio.py
@@ -0,0 +1,93 @@
+#!usr/bin/python2
+#Created by Harrison Gieselman and Lewis Callaway
+
+from time import sleep
+from Adafruit_Si4713 import Adafruit_Si4713
+
+FMSTATION = 10230  
+TRANSMITPOWER = 115
+
+radio = Adafruit_Si4731
+	
+	
+if not radio.begin():
+
+
+
+
+	print "error! couldn't begin!"
+	
+	
+	radio.setTXpower(POWER)
+
+
+
+
+radio.tuneFM(FMSTATION)
+
+
+
+
+
+
+
+	radio.beginRDS()
+
+
+	radio.setRDSbuffer(“makezine.com”)
+
+
+
+
+
+
+
+	while True:
+
+		printInfo()
+
+
+
+
+
+
+
+		radio.setRDSstation("Make")
+
+
+		sleep(10)
+
+
+
+
+
+
+
+		radio.setRDSstation("Internet")
+
+
+		sleep(5)
+
+
+
+
+
+
+
+		radio.setRDSstation("Radio")
+
+
+		sleep(5)
+
+
+
+
+
+
+
+		radio.setRDSstation("Stream")
+
+
+		sleep(5)
+
+
